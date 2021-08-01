caf8a8ba8386fbe2bbd7f06723c78cab5d6c588b
João Sátiro <joao.v.satiro@gmail.com>
2021-08-01 19:22:16 -0300

    add rules

---
+++ gatitobook/src/app/home/novo-usuario/minusculo.validator.ts
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+import { AbstractControl } from '@angular/forms';
+
+export function minusculoValidator(control: AbstractControl) {
+  const valor = control.value as string;
+  if (valor !== valor.toLowerCase()) {
+    return { minusculo: true };
+  } else {
+    return null;
+  }
+}
--- gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
+++ gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/unbound-method */
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { minusculoValidator } from './minusculo.validator';
 import { NovoUsuario } from './novo-usuario';
 import { NovoUsuarioService } from './novo-usuario.service';
 
@@ -15,9 +17,9 @@ export class NovoUsuarioComponent implements OnInit {
 
   ngOnInit(): void {
     this.novoUsuarioForm = this.formBuilder.group({
-      email: [''],
-      fullName: [''],
-      userName: [''],
+      email: ['', [Validators.required, Validators.email]],
+      fullName: ['', [Validators.required, Validators.minLength(4)]],
+      userName: ['', [minusculoValidator]],
       password: [''],
     });
   }
