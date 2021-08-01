5e473ea74d573332ea9248ea557137db55c863c1
João Sátiro <joao.v.satiro@gmail.com>
2021-07-31 21:54:51 -0300

    add rules autenticathion

--- gatitobook/src/app/home/login/login.component.ts
+++ gatitobook/src/app/home/login/login.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AutenticacaoService } from 'src/app/autenticacao/autenticacao.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  usuario = '';
+  senha = '';
 
-  constructor() { }
+  constructor(private authService: AutenticacaoService, private router: Router) {}
 
-  ngOnInit(): void {
+  login(): void {
+    this.authService.autenticar(this.usuario, this.senha).subscribe(
+      () => {
+        void this.router.navigate(['animais']);
+      },
+      () => {
+        alert('Usuário ou senha inválido!');
+      }
+    );
   }
-
 }
