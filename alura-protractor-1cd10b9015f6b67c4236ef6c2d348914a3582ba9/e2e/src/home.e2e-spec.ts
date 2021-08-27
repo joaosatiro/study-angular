d089feb29ed5b333dc90f069ed70ff1148f2fb52
João Sátiro <joao.v.satiro@gmail.com>
2021-08-26 22:24:02 -0300

    aprendendo...

--- alura-protractor-1cd10b9015f6b67c4236ef6c2d348914a3582ba9/e2e/src/home.e2e-spec.ts
+++ alura-protractor-1cd10b9015f6b67c4236ef6c2d348914a3582ba9/e2e/src/home.e2e-spec.ts
@@ -1,9 +1,33 @@
-import { browser } from 'protractor';
+import { browser, element, by, protractor } from 'protractor';
 
 describe('Home page', () => {
   it(`Should navigate to user profile`, async () => {
     await browser.get(`${browser.baseUrl}/#/user/flavio`);
     const title = await browser.getTitle();
     expect(title).toEqual('Timeline');
+  });
+
+  it(`Should display a list of photos`, async () => {
+    await browser.get(`${browser.baseUrl}/#/user/flavio`);
+    const list = element.all(by.css('.photo'));
+    const photoListSize = await list.count();
+    expect(photoListSize).toBeGreaterThan(0);
+  });
+
+  it(`Should navigate to photo detail when photo navigation is trigered`, async () => {
+    await browser.get(`${browser.baseUrl}/#/user/flavio`);
+    const firstElement = element.all(by.css('.photo')).first();
+    await firstElement.sendKeys(protractor.Key.ENTER);
+    const title = await browser.getTitle();
+    expect(title).toBe('Photo detail');
+  });
+
+  it(`Should list one item when filtering by word "farol"`, async () => {
+    await browser.get(`${browser.baseUrl}/#/user/flavio`);
+    const searchInput = element(by.css('ap-search input[type=search]'));
+    await searchInput.sendKeys('farol');
+    const list = element.all(by.css('.photo'));
+    const photoListSize = await list.count();
+    expect(photoListSize).toBe(1);
   });
 });
